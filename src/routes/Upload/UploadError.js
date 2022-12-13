4a478df75feae994e034589bfd1bb0894fd7ce97
Adam Alton <270255+adamalton@users.noreply.github.com>
2022-12-13 17:24:29 +0000

    fix: various copy tweaks and fixes

--- src/routes/Upload/UploadError.js
+++ src/routes/Upload/UploadError.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function UploadError() {
   return (
     <div>
-      <h1 className="">Ups! Something did go wrong! Please try again later!</h1>
+      <h1 className="">Oops! Something went wrong! Please try again later!</h1>
       <p>
         <Link to="/upload">Go back to uploads</Link>
       </p>
