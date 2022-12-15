9090d6f357c0dd3543920c8ed4827fcc3e1c8d1c
Paolo <paolo@potatolondon.com>
2022-12-15 16:37:21 +0000

    Polish dashboard

--- src/routes/Upload/UploadSuccess.js
+++ src/routes/Upload/UploadSuccess.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Navigate, useHref, useOutletContext } from "react-router-dom";
 import CopyText from "../../components/CopyText/CopyText";
+import { useAbsoluteHref } from "../../utils";
 
 export default function UploadSuccess() {
   const [files, setFiles, dataCid, setDataCid] = useOutletContext(); // eslint-disable-line no-unused-vars
 
-  const link = `${window.location.protocol}//${window.location.host}${useHref(
-    `/download/${dataCid}`
-  )}`;
+  const link = useAbsoluteHref(`/download/${dataCid}`);
 
   if (!dataCid) {
     return <Navigate to="/upload" />;
