0bf520940be7308027e8754d65abe31787c0feb9
joshJarr <josh.jarvis@potatolondon.com>
2022-12-16 11:30:29 +0000

    fix: Style copy to clipboard component

--- src/components/CopyText/CopyText.js
+++ src/components/CopyText/CopyText.js
@@ -11,19 +11,31 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
  * @returns
  */
 export default function CopyText({ text, textToBeDisplayed }) {
+
   const [linkCopied, setLinkCopied] = useState(false);
+
+  // Triggers the check mark to visually feedback the link is copied.
+  const triggerCopied = () => {
+    setLinkCopied(true)
+    setTimeout(() => {
+      setLinkCopied(false)
+    }, 2000)
+  }
+
   return (
-    <div className="dib ba br2 pa1 ph2">
-      <span>{textToBeDisplayed ? textToBeDisplayed : text}</span>
-      <CopyToClipboard text={text} onCopy={() => setLinkCopied(true)}>
+    <div className="dib ba br2 pa3 pl2 pr2 w-100 flex">
+      <div className="overflow-x-scroll">
+        <span className="code lh-copy">{textToBeDisplayed ? textToBeDisplayed : text}</span>
+      </div>
+      <CopyToClipboard className="ml1" text={text} onCopy={() => triggerCopied()}>
         <span>
           {" "}
           {linkCopied ? (
-            <TickIcon height={18} />
+            <TickIcon height={24} />
           ) : (
             <span>
               {" "}
-              <CopyIcon height={18} />{" "}
+              <CopyIcon stroke="white" height={24} />{" "}
             </span>
           )}
         </span>
